6620b215defc8b0bf8e5c2d433c17e981a7a6d14
Darren <79320729+itzfusiondude@users.noreply.github.com>
2022-04-07 12:09:03 +0000

    Initial commit

---
+++ main.lua
@@ -0,0 +1,18 @@
+print("Welcome To Darren's Calculator!")
+print("Type Your Numbers and Your Operation.")
+print("Ex: Num + Num")
+local function Addition(num1, num2)
+	print(num1+num2)
+end
+
+local function Subtraction(num1, num2)
+	print(num1-num2)
+end
+
+local function Multiplication(num1, num2)
+	print(num1*num2)
+end
+
+local function Division(num1, num2)
+	print(num1/num2)
+end
