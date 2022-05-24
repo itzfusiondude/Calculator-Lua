14c0ad3c93aeae45495fb3a0a4cae2c252c1fb4d
Darren <79320729+itzfusiondude@users.noreply.github.com>
2022-05-24 12:24:01 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,54 +1,21 @@
--- Problems:
--- Does Random Answers
--- After Operation is Done need to be repeated
+-- Calculator [Fixed]
+-- Credits To Timur Yucel
 
 
-print("Credits: Darren Anish = Editor, Timur Yucel = Creator")
-local seconds = os.time()
-  repeat until os.time() > seconds
-local seconds = os.time()
-  repeat until os.time() > seconds
-local seconds = os.time()
-  repeat until os.time() > seconds
-print("When Operation Is Done - You Can Still Type Another Operation")
-local seconds = os.time()
-  repeat until os.time() > seconds
-local seconds = os.time()
-  repeat until os.time() > seconds
-print("Ex: [Num1] x [Num2]")
-local seconds = os.time()
-  repeat until os.time() > seconds
-local seconds = os.time()
-print("Type First Number:")
-local num1 = io.read()
-print("Second Number:")
-local num2 = io.read()
-print("Choose Your Symbol: Add, Subtract, Multiply or Divide")
-local operator = io.read()
-if operator == "Add" or "add" then
-   local add = num1 + num2
-   print(add)
-print("Damn, You Got it Wrong! SIKE This Calculator is Supreme!")
+print("Type Your Operation.")
+print("Ex: Num + Num")
+local function Addition(num1, num2)
+	print(num1+num2)
 end
-io.read()
 
-if operator == "Subtract" or "subtract" then
-   local subtract = num1 - num2
-   print(subtract)
-print("Wow, Why Don't You Just Use a Real Calculator Bro? You  Broke?")
+local function Subtraction(num1, num2)
+	print(num1-num2)
 end
-io.read()
 
-if operator == "Multiply" or "multiply" then
-   local multiply = num1 * num2
-   print(multiply)
-print("LOL, This is The Answer...")
+local function Multiplication(num1, num2)
+	print(num1*num2)
 end
-io.read()
 
-if operator == "Divide" or "divide" then
-   local divide = num1 / num2
-   print(divide)
-print("OMG, You Cheated On Your Math Test!!!")
+local function Division(num1, num2)
+print(num1/num2)
 end
-io.read()
